65fd25445df562648544d9b8082811d7ae163876
Vinicius Soares de Sousa <120108578+ViniciusSousaDev@users.noreply.github.com>
2026-03-05 21:07:52 -0300

    Add adjacency matrix initialization and display
    Initialize adjacency matrix for a graph and print it.

---
+++ main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+#define V 5
+
+int mAdj[V][V];
+
+int main() {
+    //vertice 1
+    mAdj[0][1] = 1;
+    mAdj[0][2] = 1;
+    mAdj[0][3] = 1;    
+    //vertice 2
+    mAdj[1][2] = 1;    
+    //vertice 3
+    //vertice 4
+    mAdj[3][2] = 1;  
+    mAdj[3][4] = 1;    
+    //vertice 5
+    mAdj[4][0] = 1;
+    mAdj[4][1] = 1;    
+    
+    int i, j;
+
+    for(i = 0; i < V; i++){
+        for(j = 0; j < V; j++){
+            printf("%d\t", mAdj[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
